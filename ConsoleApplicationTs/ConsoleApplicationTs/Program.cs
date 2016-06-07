c27fb9a82e1a273685525a2630029733c8ec7174
Svetlana Shevchenko <VetaShev@yandex.ru>
2016-06-07 15:38:03 +0300

    commit 2 row

--- ConsoleApplicationTs/ConsoleApplicationTs/Program.cs
+++ ConsoleApplicationTs/ConsoleApplicationTs/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplicationTs
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("2 row");
             Console.ReadLine();
         }
     }
